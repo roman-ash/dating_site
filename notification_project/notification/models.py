76870052ce4d0151054dd5994ca475bc0921521c
Roman <ashirovm4@gmail.com>
2024-02-26 21:29:47 +0500

    br3

--- notification_project/notification/models.py
+++ notification_project/notification/models.py
@@ -1,19 +1,21 @@
 import uuid
+import pytz
 from django.core.validators import RegexValidator
 from django.db import models
 from django_unixdatetimefield import UnixDateTimeField
 
 
 class Client(models.Model):
+    TIMEZONES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phone_number = models.CharField(max_length=11, unique=True, validators=[
         RegexValidator(regex='^(7+([0-9]){10})$',
                        message='Phone number must be forman: 7XXXXXXXXXX',
                        code='invalid_phone_nuber')]
         )
-    mobile_operator_code = models.CharField(max_length=3)
+    mobile_operator_code = models.CharField(max_length=3)  # поле не отображалось в формах и заполнялось автоматически
     tag = models.CharField(max_length=50)
-    time_zone = models.CharField(max_length=10)
+    time_zone = models.CharField(max_length=50, choices=TIMEZONES, default='UTC')
     objects = models.Manager()
 
     def update_mobile_operator_code(self):
@@ -28,12 +30,19 @@ class Newsletter(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     start_date = UnixDateTimeField()
     text = models.TextField()
+    mobile_operator_code = models.CharField(max_length=3)
+    tag = models.CharField(max_length=50)
     end_date = UnixDateTimeField()
     objects = models.Manager()
 
+
 class Message(models.Model):
-    class AccountStatuses(models.TextChoices):
-        sent = 'sent'
+    class MessageStatuses(models.TextChoices):
         not_sent = 'not_sent'
+        sent = 'sent'
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    date = UnixDateTimeField()
+    status = models.CharField(max_length=8, choices=MessageStatuses.choices, default=MessageStatuses.not_sent)
+    sent_date = UnixDateTimeField(verbose_name='sent date')
+    client_id = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='messages')
+    newsletter_id = models.ForeignKey(Newsletter, on_delete=models.CASCADE, related_name='messages')
+    objects = models.Manager()
--- notification_project/notification/serializers.py
+++ notification_project/notification/serializers.py
@@ -9,12 +9,10 @@ class MessageSerializer(serializers.ModelSerializer):
 
 
 class NewsletterSerializer(serializers.ModelSerializer):
-    #matchingKey = serializers.SerializerMethodField()
-    #date = MessageSerializer(many=True)
     class Meta:
         model = Newsletter
         fields = '__all__'
-        #fields = ('id', 'start_date', 'text', 'end_date', 'message_set')
+
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
